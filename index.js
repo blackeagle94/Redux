6a60b8787ae51fcc382af4177cad4e86bd23b0cc
blackeagle94 <blackeagle4894@gmail.com>
2021-01-09 22:44:33 +0200

    created action, reducer and store then dispatched action then subscribed to store

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('From index.js')
+const redux = require('redux')
+const createStore = redux.createStore
+
+const BUY_CAKE = 'BUY_CAKE'
+
+
+const buyCake = () => {
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+
+// (prevState, action) => newState
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        default: return state
+    }
+}
+
+const store = createStore(reducer)
+console.log('Initial state', store.getState())
+const unsunscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsunscribe()
+store.dispatch(buyCake())
