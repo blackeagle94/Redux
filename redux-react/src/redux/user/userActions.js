8489e6c4b6d461756a43de1577b217ef64729c5a
blackeagle94 <blackeagle4894@gmail.com>
2021-01-10 23:07:06 +0200

    created async function that fetches users from api

--- redux-react/src/redux/user/userActions.js
+++ redux-react/src/redux/user/userActions.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS, FETCH_USERS_FAILURE } from './userTypes';
 
 export const fetchUSersRequest = () => {
@@ -19,3 +20,18 @@ export const fetchUSersFailure = (error) => {
 		payload: error
 	};
 };
+
+export const fetchUsers = () => {
+    return (dispatch) => {
+        dispatch(fetchUSersRequest())
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            const users = response.data
+            dispatch(fetchUSersSucces(users))
+        })
+        .catch(error => {
+            const errorMessage = error.message
+            dispatch(fetchUSersFailure(errorMessage))
+        })
+    }
+}
