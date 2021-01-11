3141064478762aaf5ba10c764e92737d58100306
blackeagle94 <blackeagle4894@gmail.com>
2021-01-11 12:23:29 +0200

    Started React Hooks and created file for using useState hook

--- redux-react/src/App.js
+++ redux-react/src/App.js
@@ -5,17 +5,21 @@ import IceCreamContainer from './components/IceCreamContainer';
 import ItemContainer from './components/ItemContainer';
 import NewCakeContainer from './components/NewCakeContainer';
 import UserContainer from './components/UserContainer';
+import ClassCounter from './Hooks/components/ClassCounter';
 
 function App() {
 	return (
 		<div className="App">
+			<ClassCounter />
+			{/* 
+				Redux Components
 			<ItemContainer cake />
 			<ItemContainer />
 			<CakeContainer />
 			<HooksCakeContainer />
 			<IceCreamContainer />
 			<NewCakeContainer />
-			<UserContainer />
+			<UserContainer /> */}
 		</div>
 	);
 }
