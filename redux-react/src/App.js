f7ebb5e06af8dcca1973c7faeef704fdf4aded83
blackeagle94 <blackeagle4894@gmail.com>
2021-01-11 13:32:05 +0200

    cleanend unnecessary lines from App.js

--- redux-react/src/App.js
+++ redux-react/src/App.js
@@ -5,15 +5,12 @@ import IceCreamContainer from './components/IceCreamContainer';
 import ItemContainer from './components/ItemContainer';
 import NewCakeContainer from './components/NewCakeContainer';
 import UserContainer from './components/UserContainer';
-import ClassCounter from './Hooks/components/ClassCounter';
-import HookCounter from './Hooks/components/HookCounter';
-import HookCounter2 from './Hooks/components/HookCounter2';
-import HookCounter4 from './Hooks/components/HookCounter4';
+
 
 function App() {
 	return (
 		<div className="App">
-			<HookCounter4 />
+
 			{/* 
 				Redux Components
 			<ItemContainer cake />
