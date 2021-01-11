4718475fa4e8049702a62b9ad629f03da0b88aaa
blackeagle94 <blackeagle4894@gmail.com>
2021-01-11 22:44:49 +0200

    learned createContext method

--- redux-react/src/App.js
+++ redux-react/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import './App.css';
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
@@ -5,14 +7,19 @@ import IceCreamContainer from './components/IceCreamContainer';
 import ItemContainer from './components/ItemContainer';
 import NewCakeContainer from './components/NewCakeContainer';
 import UserContainer from './components/UserContainer';
+import ComponentC from './Hooks/context API/ComponentC';
 import DataFetching from './Hooks/useEffect/DataFetching';
 
-
-
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
 function App() {
 	return (
 		<div className="App">
-			<DataFetching />
+			<UserContext.Provider value={'ismail'}>
+				<ChannelContext.Provider value={'code'}>
+					<ComponentC />
+				</ChannelContext.Provider>
+			</UserContext.Provider>
 			{/* Redux Components
 			<ItemContainer cake />
 			<ItemContainer />
